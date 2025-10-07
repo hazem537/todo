4d604e1ec1fbe4f7e85b53431a8068a5e6e0d442
hazem hamdy <hh4morsy@gmail.com>
2025-10-07 15:03:54 +0300

    update conf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/ui','@prisma/nuxt']
-})
+  modules: ["@nuxt/ui", "@prisma/nuxt"],
+  vite: {
+    resolve: {
+      alias: {
+        ".prisma/client/index-browser":
+          "./node_modules/.prisma/client/index-browser.js",
+      },
+    },
+  },
+});
